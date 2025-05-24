669fda0e3ce06d914a6de215f8aaac3acdf125bf
Nikhil Dhaliya <nikhil11754@gmail.com>
2025-05-24 09:43:03 +0530

    Add backend package.json and update server configuration for deployment

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? 'https://yourdomain.com'
+    ? 'elegant-ecommerce-7fyo.vercel.app'
     : 'http://localhost:5173',
   credentials: true  //without them cookies won't be sent
 }));
@@ -29,7 +29,9 @@ app.use("/api/admin", AdminRoutes);
 app.use("/api", productRoutes);
 app.use("/api/cart", cartRoutes);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server is running on port 3000.");
+  console.log(`Server is running on port ${PORT}`);
 });
